ce16b237706e6000172ee49ff0278d9da79c302f
PreetySethy <sethy.preety@gmail.com>
2020-07-21 03:57:05 +0300

    3rd commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+var message = $("#message")
+	var name = $("#name")
+  var rating = $("#rating")
+  var send_data = $("#send_data")
+	var update = $("#update")
+
 app.get('/', (req, res) => {
   res.send('<h1>Hey Socket.io</h1>');
 });
@@ -14,7 +20,14 @@ io.on('connection', (socket) => {
   socket.on('my message', (msg) => {
     console.log('message: ' + msg);
     io.emit('my broadcast', `server: ${msg}`);
+    send_data.append("" + msg.name + ": " + msg.rating + "")
   });
+
+  //Emit message
+	send_data.click(function(){
+		socket.emit('new_message', {message : message.val()})
+	})
+
 });
 
 http.listen(3000, () => {
